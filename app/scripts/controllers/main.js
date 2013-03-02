d4d7fccfd9659e921dfe5cf0945d792a95060b4c
Tony Hill <designmite@gmail.com>
2013-03-02 12:12:28 +0000

    yeoman angular scaffold

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+whistlingMonster2App.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
---
+++ app/scripts/whistling-monster-2.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var whistlingMonster2App = angular.module('whistlingMonster2App', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
